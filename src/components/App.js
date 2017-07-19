bc1e80613bef34edc61a0ceaa247f3c9e748f1e1
Eric Loos <ericloos00@gmail.com>
2017-07-19 18:19:16 -0500

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,60 @@
 import React from 'react';
 
 import FruitBasket from './FruitBasket';
+import Filter from './Filter';
 
-const App = () => <FruitBasket />;
+// const App = () => <FruitBasket />;
+
+class App extends React.Component {
+
+constructor(props) {
+  super(props);
+
+  this.state = {
+    fruit: [],
+    filters: [],
+    currentFilter: null
+  }
+
+    this.fetchFilters = this.fetchFilters.bind(this);
+    this.updateFilter = this.updateFilter.bind(this);
+  }
+
+  componentWillMount() {
+    this.fetchFilters();
+    this.fetchFruit();
+  }
+
+  fetchFilters() {
+    fetch('/api/fruit_types')
+      .then(res => res.json())
+      .then(filters => this.setState({filters: filters}));
+  }
+
+  fetchFruit() {
+    fetch('/api/fruit')
+      .then(res => res.json())
+      .then(fruit => this.setState({ fruit: fruit }));
+  }
+
+  updateFilter(e) {
+    console.log('update filter to: ', e.target.value);
+    this.setState({ currentFilter: e.target.value });
+  }
+
+
+
+render() {
+  return <div>
+  <FruitBasket fruit={this.state.fruit} 
+  filters={this.state.filters} 
+  currentFilter={this.state.currentFilter}
+  updateFilterCallback={this.updateFilter}/>
+  </div>
+}
+
+
+
+}
 
 export default App;
